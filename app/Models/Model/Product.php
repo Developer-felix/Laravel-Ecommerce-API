417df61d31a113a40a7a1f01f7599c45dc829920
Developer-felix <onjombafelix1@gmail.com>
2022-03-25 04:20:56 -0700

    Created the relationships and tested

--- app/Models/Model/Product.php
+++ app/Models/Model/Product.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Models\Model;
+use App\Models\Model\Review;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Model/Review.php
+++ app/Models/Model/Review.php
@@ -4,8 +4,12 @@ namespace App\Models\Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Model\Product;
 
 class Review extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    public function product(){
+       return $this->belongsTo(Product::class);
+    }
 }
